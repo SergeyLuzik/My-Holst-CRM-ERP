f850380faf188c1630ef7d2827a27610866d5920
Sergey Luzik <luziksergey@gmail.com>
2024-09-16 18:26:21 +0300

    feat: add theme

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import { createRoot } from "react-dom/client";
 
 import App from "./components/App/App.jsx";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./themes/light.js";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={lightTheme}>
+    <App />
+  </ThemeProvider>
+);
