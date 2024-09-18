efddb5d0bc484f4d589bd8d1c3be9eca52496ea3
SergeyLuzik <67701136+SergeyLuzik@users.noreply.github.com>
2024-09-18 15:10:52 +0000

    feat: add default styled components

---
+++ src/components/styled/button.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+export const Button = styled.button`
+display:block;
+border: 0;
+cursor:pointer;
+box-shadow: none;
+background: none;
+&:hover, &:active {
+background: none;
+box-shadow: none;
+}
+`;
