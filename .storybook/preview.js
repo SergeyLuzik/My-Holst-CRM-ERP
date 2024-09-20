bc6ff8c70d18409a5ce6508c936081ce73d802ce
Sergey Luzik <luziksergey@gmail.com>
2024-09-20 10:20:01 +0300

    feat: add styled-component ThemeProvider with lightTheme as global decorator for storeys

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,16 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "../src/themes/light";
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={lightTheme}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
   parameters: {
     controls: {
       matchers: {
