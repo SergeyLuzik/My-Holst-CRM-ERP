f0d809ab15023298bfdb0ed827f083b264114a24
Sergey Luzik <luziksergey@gmail.com>
2024-09-21 06:33:42 +0300

    feat: add variants to the Status story

--- src/components/ui/status/Status.stories.jsx
+++ src/components/ui/status/Status.stories.jsx
@@ -7,7 +7,27 @@ export default {
 
 const Template = (args) => <Status {...args} />;
 
-export const OrderStatus = Template.bind({});
-OrderStatus.args = {
+export const New = Template.bind({});
+New.args = {
   type: "new",
+};
+
+export const Approve = Template.bind({});
+Approve.args = {
+  type: "approve",
+};
+
+export const Printed = Template.bind({});
+Printed.args = {
+  type: "printed",
+};
+
+export const Done = Template.bind({});
+Done.args = {
+  type: "done",
+};
+
+export const Complete = Template.bind({});
+Complete.args = {
+  type: "complete",
 };
