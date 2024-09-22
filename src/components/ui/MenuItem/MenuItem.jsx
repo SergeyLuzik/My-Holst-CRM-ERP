8f1eee026c8c9585d0703994a277c2853d0217f9
Sergey Luzik <luziksergey@gmail.com>
2024-09-22 21:15:25 +0300

    ref: adв props to MenuItem

--- src/components/ui/MenuItem/MenuItem.jsx
+++ src/components/ui/MenuItem/MenuItem.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import * as S from "./styles";
-export const MenuItem = () => {
-  return <S.MenuItem />;
+export const MenuItem = ({ icon, href, text }) => {
+  return (
+    <S.MenuItem icon={icon} href={href}>
+      ${text}
+    </S.MenuItem>
+  );
 };
--- src/components/ui/MenuItem/styles.js
+++ src/components/ui/MenuItem/styles.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { Link } from "../../styled";
-import icon from "../../../assets/icons/picture.svg";
 
 export const MenuItem = styled(Link)`
   font-size: 20px;
@@ -14,7 +13,7 @@ export const MenuItem = styled(Link)`
     width: 24px;
     height: 24px;
     content: "";
-    mask: url(${icon});
+    mask: url(${({ icon }) => icon});
     background-color: currentColor;
   }
   &:hover {
