358aa25cd0a2b5ed4ea659abf7a955774c11e3d4
Sergey Luzik <luziksergey@gmail.com>
2024-09-22 20:55:07 +0300

    ref: rename the status folder with a capital latter

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Status } from "../ui/status/Status";
+import { Status } from "../ui/Status/Status";
 import { GlobalStyle } from "./styles";
+import { MenuItem } from "../ui/MenuItem/styles";
 export const App = () => {
   return (
     <>
       <GlobalStyle />
       <Status type="done" />
+      <MenuItem>Картины</MenuItem>
     </>
   );
 };
