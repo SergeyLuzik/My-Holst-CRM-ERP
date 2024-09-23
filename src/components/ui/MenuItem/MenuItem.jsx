5895ca84684cc53bb2908ad3eaa01f34c103447b
Sergey Luzik <luziksergey@gmail.com>
2024-09-23 09:01:59 +0300

    fix: MenuItem text prop transfer

--- src/components/ui/MenuItem/MenuItem.jsx
+++ src/components/ui/MenuItem/MenuItem.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import * as S from "./styles";
 export const MenuItem = ({ icon, href, text }) => {
   return (
-    <S.MenuItem icon={icon} href={href}>
-      ${text}
+    <S.MenuItem $icon={icon} href={href}>
+      {text}
     </S.MenuItem>
   );
 };
