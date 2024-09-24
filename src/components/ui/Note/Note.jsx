a4ffb8c429587f2106e2f9d14749069e4f7139e9
Sergey Luzik <luziksergey@gmail.com>
2024-09-24 08:34:53 +0300

    ref: change Note structure

--- src/components/ui/Note/Note.jsx
+++ src/components/ui/Note/Note.jsx
@@ -1,3 +1,9 @@
 import React from "react";
 import * as S from "./styles";
-export const Note = ({ text }) => <S.Note defaultValue={text} />;
+import { CloseButton } from "../CloseButton/CloseButton";
+export const Note = ({ text }) => (
+  <S.NoteWrapper>
+    <S.Text contentEditable="true">{text}</S.Text>
+    <CloseButton />
+  </S.NoteWrapper>
+);
--- src/components/ui/Note/styles.js
+++ src/components/ui/Note/styles.js
@@ -1,14 +1,24 @@
 import styled from "styled-components";
-import { Input } from "../../styled";
+//import { Input } from "../../styled";
+import icon from "../../../assets/icons/plus.svg";
+import { P } from "../../styled";
 
-export const Note = styled(Input)`
-  font-size: 12px;
-  font-weight: ${({ theme }) => theme.fontWeights.semiBold};
-  color: ${({ theme }) => theme.colors.main};
+export const NoteWrapper = styled.div`
+  display: inline-flex;
+  gap: 8px;
+  align-items: center;
   padding-inline: ${({ theme }) => theme.spacings.s};
   padding-block: ${({ theme }) => theme.spacings.xs};
   border-radius: ${({ theme }) => theme.radiuses.s};
   background-color: ${({ theme }) => theme.colors.extraLiteMain};
+`;
+export const Text = styled(P)`
+  max-width: 167px;
+  font-size: 12px;
+  font-weight: ${({ theme }) => theme.fontWeights.semiBold};
+  color: ${({ theme }) => theme.colors.main};
+  padding: ${({ theme }) => theme.spacings.xxs};
+  border-radius: ${({ theme }) => theme.radiuses.s};
   &:hover {
     cursor: pointer;
   }
