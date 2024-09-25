3ed98e88e195fee6dd1d17ed251694e489fae679
Sergey Luzik <luziksergey@gmail.com>
2024-09-25 07:13:53 +0300

    ref: add SortButton

---
+++ src/components/ui/SortButton/SortButton.stories.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { SortButton } from "./SortButton";
+
+export default {
+  title: "Кнопка сортировки",
+};
+
+export const Sort = () => <SortButton />;
---
+++ src/components/ui/SortButton/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { Button } from "../../styled-tags";
+import { flexCenter, squareButton } from "../../../style-fragments";
+import sort from "../../../assets/icons/sort.svg";
+
+export const SortButton = styled(Button)`
+  ${flexCenter};
+  ${squareButton}
+  &::before {
+    width: 16px;
+    height: 12px;
+    mask: url(${sort});
+  }
+`;
