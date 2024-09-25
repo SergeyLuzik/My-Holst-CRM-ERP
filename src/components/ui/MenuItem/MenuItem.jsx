d9819d66358d4e3ed80fc5b4e4be3c546c3dd374
Sergey Luzik <luziksergey@gmail.com>
2024-09-25 10:33:54 +0300

    ref: change MenuItem structure

--- src/components/ui/MenuItem/MenuItem.jsx
+++ src/components/ui/MenuItem/MenuItem.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import * as S from "./styles";
-export const MenuItem = ({ icon, href, text, Option }) => {
-  return (
-    <S.MenuItem $icon={icon} href={href}>
-      <>
-        {text}
-        {Option && Option()}
-      </>
-    </S.MenuItem>
-  );
-};
+export const MenuItem = ({ icon, href, text, Option }) => (
+  <S.MenuItem $icon={icon} href={href}>
+    <>
+      {text}
+      {Option && Option()}
+    </>
+  </S.MenuItem>
+);
