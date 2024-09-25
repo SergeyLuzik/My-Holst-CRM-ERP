251e8019487ef70f8db084954ecd656e7d6569aa
Sergey Luzik <luziksergey@gmail.com>
2024-09-25 07:13:12 +0300

    ref: add OrientationButton

---
+++ src/components/ui/OrientationButton/OrientationButton.stories.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { OrientationButton } from "./OrientationButton";
+
+export default {
+  title: "Кнопка смены ориентации",
+};
+
+export const Orientation = () => <OrientationButton />;
---
+++ src/components/ui/OrientationButton/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import { Button } from "../../styled-tags";
+import { flexCenter, squareButton } from "../../../style-fragments";
+import portrait from "../../../assets/icons/portrait.svg";
+import landscape from "../../../assets/icons/landscape.svg";
+
+export const OrientationButton = styled(Button)`
+  ${flexCenter};
+  ${squareButton}
+  &::before {
+    width: 16px;
+    height: 16px;
+    mask: url(${portrait});
+  }
+`;
