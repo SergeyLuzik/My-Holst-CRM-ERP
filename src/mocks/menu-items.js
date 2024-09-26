51db35876c949b9f4da771555361e6ada1838f9c
Sergey Luzik <luziksergey@gmail.com>
2024-09-26 09:31:37 +0300

    add mock for Menu

--- src/mocks/menu-items.js
+++ src/mocks/menu-items.js
@@ -1,1 +1,22 @@
-export const menuItems = [];
+import picture from "../assets/icons/picture.svg";
+import polygraphy from "../assets/icons/polygraphy.svg";
+import other from "../assets/icons/other.svg";
+import cash from "../assets/icons/cash.svg";
+import needs from "../assets/icons/needs.svg";
+import notes from "../assets/icons/notes.svg";
+import { PlusButton } from "../components/ui/PlusButton/PlusButton";
+import { Triangle } from "../components/ui/TriangleButton/TriangleButton.stories";
+export const menuItems = [
+  { id: 1, icon: picture, href: "#", text: "Картины", Option: PlusButton },
+  {
+    id: 2,
+    icon: polygraphy,
+    href: "#",
+    text: "Полиграфия",
+    Option: PlusButton,
+  },
+  { id: 3, icon: other, href: "#", text: "Прочие", Option: PlusButton },
+  { id: 4, icon: cash, href: "#", text: "Касса", Option: null },
+  { id: 5, icon: needs, href: "#", text: "Потребности", Option: null },
+  { id: 6, icon: notes, href: "#", text: "Заметки", Option: Triangle },
+];
