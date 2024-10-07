e53191281e0fc166bfba712352ad4a291c7ec1c5
Sergey Luzik <luziksergey@gmail.com>
2024-10-07 10:38:22 +0300

    feat: start to add days scroll in RaysRange

--- src/components/blocks/DaysRange/index.jsx
+++ src/components/blocks/DaysRange/index.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import * as S from "./styles";
 import { Day } from "../../ui/Day";
 import { MonthSelect } from "../../ui/MonthSelect";
 
 import { days } from "../../../mocks/days";
-export const DaysRange = ({ children }) => (
-  <S.DaysRange>
-    <MonthSelect />
-    <S.DaysList>
-      {days.map(({ day, type }) => (
-        <S.Day key={day}>
-          <Day day={day} type={type} />
-        </S.Day>
-      ))}
-    </S.DaysList>
-  </S.DaysRange>
-);
+
+export const DaysRange = ({ children }) => {
+  const daysRef = useRef(null);
+  const [daysPosition, setDaysPosition] = useState(0);
+  const handleDaysScroll = (e) => {
+    //setDaysPosition(e.target);
+    console.log(e);
+  };
+  return (
+    <S.DaysRange>
+      <MonthSelect />
+      <S.DaysList onWheel={handleDaysScroll}>
+        {days.map(({ day, type }) => (
+          <S.Day key={day}>
+            <Day day={day} type={type} />
+          </S.Day>
+        ))}
+      </S.DaysList>
+    </S.DaysRange>
+  );
+};
