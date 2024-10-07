2be97bc58d6c1c1b656ef06b6ed20f02f7a3fbfb
Sergey Luzik <luziksergey@gmail.com>
2024-10-07 10:24:52 +0300

    fix: days overflow in DaysRange

--- src/components/blocks/DaysRange/styles.js
+++ src/components/blocks/DaysRange/styles.js
@@ -2,13 +2,15 @@ import styled from "styled-components";
 import { Li, Ul } from "../../../styled-tags";
 
 export const DaysRange = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: min-content 1fr;
+  grid-template-rows: max-content;
   background-color: ${({ theme }) => theme.colors.white};
   border-radius: ${({ theme }) => theme.radiuses.xl};
   gap: ${({ theme }) => theme.spacings.l};
   padding: ${({ theme }) => theme.spacings.s};
   width: 100%;
-  overflow: hidden;
+  overflow-x: hidden;
 `;
 
 export const DaysList = styled(Ul)`
@@ -17,5 +19,6 @@ export const DaysList = styled(Ul)`
 `;
 
 export const Day = styled(Li)`
-  display: block;
+  display: flex;
+  flex-shrink: 0;
 `;
