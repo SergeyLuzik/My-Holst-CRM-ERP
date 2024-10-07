131b7f951352b438da0bb9e2da618c1c081a8ee7
Sergey Luzik <luziksergey@gmail.com>
2024-10-07 10:05:36 +0300

    ref: simplify Day code

--- src/components/ui/Day/index.jsx
+++ src/components/ui/Day/index.jsx
@@ -1,5 +1,3 @@
 import React from "react";
 import * as S from "./styles";
-export const Day = ({ day, type }) => {
-  return <S.Day $type={type}>{day}</S.Day>;
-};
+export const Day = ({ day, type }) => <S.Day $type={type}>{day}</S.Day>;
