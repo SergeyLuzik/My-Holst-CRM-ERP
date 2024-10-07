cd541d65b116112fad1ebf3d1c3829dcaa01bee3
Sergey Luzik <luziksergey@gmail.com>
2024-10-07 10:08:22 +0300

    feat: add dynamic render of  Days to DaysRange

--- src/components/blocks/DaysRange/index.jsx
+++ src/components/blocks/DaysRange/index.jsx
@@ -3,21 +3,16 @@ import * as S from "./styles";
 import { Day } from "../../ui/Day";
 import { MonthSelect } from "../../ui/MonthSelect";
 
+import { days } from "../../../mocks/days";
 export const DaysRange = ({ children }) => (
   <S.DaysRange>
     <MonthSelect />
-    <Day day="Вторник, 31 июля" type="past" />
-    <Day day="Вторник, 31 июля" type="past" />
-    <Day day="Вторник, 31 июля" type="past" />
-    <Day day="Вторник, 31 июля" type="past" />
-    <Day day="Вторник, 31 июля" type="past" />
-    <Day day="Вторник, 31 июля" type="past" />
-    <Day day="Вторник, 31 июля" type="past" />
-    <Day day="Вторник, 31 июля" type="past" />
-    <Day day="Вторник, 31 июля" type="today" />
-    <Day day="Вторник, 31 июля" />
-    <Day day="Вторник, 31 июля" type="selected" />
-    <Day day="Вторник, 31 июля" />
-    <Day day="Вторник, 31 июля" />
+    <S.DaysList>
+      {days.map(({ day, type }) => (
+        <S.Day key={day}>
+          <Day day={day} type={type} />
+        </S.Day>
+      ))}
+    </S.DaysList>
   </S.DaysRange>
 );
