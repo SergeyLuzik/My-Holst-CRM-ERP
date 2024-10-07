a239c8cd2c722f77468f772d2ae3942485d1f806
Sergey Luzik <luziksergey@gmail.com>
2024-10-07 21:04:44 +0300

    feat: add generation of past days when scrolling DaysRange to the left

--- src/components/blocks/DaysRange/index.jsx
+++ src/components/blocks/DaysRange/index.jsx
@@ -3,25 +3,30 @@ import * as S from "./styles";
 import { Day } from "../../ui/Day";
 import { MonthSelect } from "../../ui/MonthSelect";
 
-import { days } from "../../../mocks/days";
+import { futureDays } from "../../../mocks/futureDays";
+
+const setDay = () => ({ day: Date.now(), type: "past" });
 
 export const DaysRange = ({ children }) => {
   const daysRef = useRef(null);
   const [daysPosition, setDaysPosition] = useState(0);
-  // todo использовать useState для хранения массива дней,
-  // todo setDays добавляет в массив дни в начало
+
+  const [days, setDays] = useState(futureDays);
   const handleDaysScroll = (e) => {
-    setDaysPosition((prev) => prev + e.deltaX);
-    // todo при скролле вправо добавлять прошедние дни через setDays
+    setDaysPosition((prev) => {
+      if (prev > e.deltaY) {
+        // при скролле влево добавлять прошедние дни через setDays
+        setDays([setDay(prev + e.deltaY), ...days]); //  setDays добавляет в массив дни в начало
+      }
+      return prev + e.deltaY;
+    });
+    console.log(e.deltaX, e.deltaY);
   };
   return (
     <S.DaysRange>
       <MonthSelect />
-      <S.DaysWrapper>
-        <S.DaysList
-          onWheel={handleDaysScroll}
-          style={{ transform: `translateX(${daysPosition}px)` }}
-        >
+      <S.DaysWrapper onWheel={handleDaysScroll}>
+        <S.DaysList style={{ transform: `translateX(${daysPosition}px)` }}>
           {days.map(({ day, type }) => (
             <S.Day key={day}>
               <Day day={day} type={type} />
