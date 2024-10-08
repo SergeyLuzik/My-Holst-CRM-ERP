560ccff03a862283aa32164d1b8cf39cc932420d
Sergey Luzik <luziksergey@gmail.com>
2024-10-08 08:55:12 +0300

    feat: add DaysList  scrolling stop when scrolling left with not enough days

--- src/components/blocks/DaysRange/index.jsx
+++ src/components/blocks/DaysRange/index.jsx
@@ -8,25 +8,45 @@ import { futureDays } from "../../../mocks/futureDays";
 const setDay = () => ({ day: Date.now(), type: "past" });
 
 export const DaysRange = ({ children }) => {
+  const daysWrapperRef = useRef(null);
   const daysRef = useRef(null);
-  const [daysPosition, setDaysPosition] = useState(0);
 
+  const [daysPosition, setDaysPosition] = useState(0);
   const [days, setDays] = useState(futureDays);
+
   const handleDaysScroll = (e) => {
+    const wrapperRect = daysWrapperRef.current.getBoundingClientRect();
+    const daysRect = daysRef.current.getBoundingClientRect();
+    //console.log("wrapperRect", wrapperRect.right);
+    //console.log("daysRect", daysRect.right);
+
     setDaysPosition((prev) => {
-      if (prev > e.deltaY) {
-        // при скролле влево добавлять прошедние дни через setDays
+      const newDaysPosition = prev + e.deltaY;
+      console.log("prev", prev, "new", newDaysPosition);
+
+      if (prev < newDaysPosition) {
+        console.log("вправо", e.deltaX, e.deltaY);
+        // при скролле вправо добавлять прошедние дни через setDays
         setDays([setDay(prev + e.deltaY), ...days]); //  setDays добавляет в массив дни в начало
+      }
+      if (prev > newDaysPosition) {
+        console.log("влево", e.deltaX, e.deltaY);
+        if (wrapperRect.right > daysRect.right) {
+          return prev;
+        }
       }
       return prev + e.deltaY;
     });
-    console.log(e.deltaX, e.deltaY);
   };
+
   return (
     <S.DaysRange>
       <MonthSelect />
-      <S.DaysWrapper onWheel={handleDaysScroll}>
-        <S.DaysList style={{ transform: `translateX(${daysPosition}px)` }}>
+      <S.DaysWrapper ref={daysWrapperRef} onWheel={handleDaysScroll}>
+        <S.DaysList
+          ref={daysRef}
+          style={{ transform: `translateX(${daysPosition}px)` }}
+        >
           {days.map(({ day, type }) => (
             <S.Day key={day}>
               <Day day={day} type={type} />
