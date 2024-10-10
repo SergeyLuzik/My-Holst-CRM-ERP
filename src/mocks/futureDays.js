4950d2b32b1af9c5f11dd483a97dfe03013aa0a8
SergeyLuzik <67701136+SergeyLuzik@users.noreply.github.com>
2024-10-10 15:43:37 +0000

    ref: add dynamic creation of futureDays

--- src/mocks/futureDays.js
+++ src/mocks/futureDays.js
@@ -1,18 +1,44 @@
-export const futureDays = [
+import { formatDate } from "../utils/formatDate";
+export const futureDays = setFutureDays();
+
+function getFutureDays(daysAmount) {
+  const todayDate = new Date();
+  const todayDay = todayDate.getDate();
+  const result = [];
+
+  for (let i = 0; i <= daysAmount; i++) {
+    const day = new Date(todayDate.setDate(todayDay + i));
+    result.push(formatDate(day));
+  }
+  return result;
+}
+
+function setFutureDays() {
+  const futureDays = getFutureDays(3);
+  const orders = [35, 20, 5];
+  const types = ["today", "selected"];
+  let result = [];
+  for (let i = 0; i < 3; i++) {
+    let day = {};
+    day.day = futureDays[i];
+    if (types[i]) {
+      day.type = types[i];
+    }
+    if (orders[i]) {
+      day.ordersCount = orders[i];
+    }
+    result.push(day);
+  }
+
+  return result;
+}
+
+/*
+[
   { day: "Четверг, 9 августа", type: "today", ordersCount: 35 },
   { day: "Пятница, 10 августа", type: "selected", ordersCount: 25 },
   { day: "Суббота, 11 августа", ordersCount: 5 },
   { day: "Воскресенье, 12 августа" },
 ];
 
-/*
-  { day: "Вторник, 31 июля", type: "past" },
-  { day: "Среда, 1 августа", type: "past" },
-  { day: "Четверг, 2 августа", type: "past" },
-  { day: "Пятница, 3 августа", type: "past" },
-  { day: "Суббота, 4 августа", type: "past" },
-  { day: "Воскресенье, 5 августа", type: "past" },
-  { day: "Понедельник, 6 августа", type: "past" },
-  { day: "Вторник, 7 августа", type: "past" },
-  { day: "Среда, 8 августа", type: "today" },
-    */
+*/
