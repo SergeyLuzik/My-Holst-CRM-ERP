f29bc8ce56f59db64e5d0a91472860b4154e553b
Sergey Luzik <luziksergey@gmail.com>
2024-10-11 10:34:22 +0300

    feat: add plus button show in Tags when hovering over Tags

--- src/components/blocks/Tags/index.jsx
+++ src/components/blocks/Tags/index.jsx
@@ -1,4 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./styles";
+import { PlusButton } from "../../ui/PlusButton";
 
-export const Tags = ({ children }) => <S.Tags>{children}</S.Tags>;
+export const Tags = ({ children }) => {
+  const [onHover, setOnHover] = useState(false);
+  const handleOnHover = () => setOnHover((perv) => !perv);
+  return (
+    <S.Tags
+      onMouseEnter={() => setOnHover(true)}
+      onMouseLeave={() => setOnHover(false)}
+    >
+      {children}
+      {onHover && <PlusButton />}
+    </S.Tags>
+  );
+};
