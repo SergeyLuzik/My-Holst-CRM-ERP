2c166f3fe743cc94787f0b65db01f289f36d0fbd
Sergey Luzik <luziksergey@gmail.com>
2024-10-13 06:03:12 +0300

    ref: add OrderText

---
+++ src/components/ui/OrderText/index.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import * as S from "./styles";
+
+export const OrderText = ({ text }) => <S.OrderText>{text}</S.OrderText>;
---
+++ src/components/ui/OrderText/styles.js
@@ -0,0 +1,13 @@
+import styled, { css } from "styled-components";
+import { P } from "../../../styled-tags";
+import { pill } from "../../../style-fragments";
+
+export const OrderText = styled(P)`
+  ${pill};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.extraLiteMain};
+    cursor: pointer;
+    border-radius: ${({ theme }) => theme.radiuses.m};
+  }
+`;
