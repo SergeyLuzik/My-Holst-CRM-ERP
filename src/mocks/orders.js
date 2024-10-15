8175953e247ca1544db5e938068549f0d014ee0d
Sergey Luzik <luziksergey@gmail.com>
2024-10-15 08:46:52 +0300

    feat: add orders mock

---
+++ src/mocks/orders.js
@@ -0,0 +1,22 @@
+export const orders = [
+  {
+    id: 1,
+    date: "05.10.2024",
+    status: "new",
+    size: "100x100",
+    style: "Сканирование",
+    description: "Свадебная пара и мама, на фоне буквы, декарация с цветами",
+    package: "ШИР ТЕМНО-ЖЕЛТАЯ",
+    tags: [
+      { type: "time", text: "15:00" },
+      { type: "important", text: "Багет 3222" },
+      { type: "important", text: "Лак" },
+      { type: "common", text: "Комментарий" },
+    ],
+    client: "Капуткина Ксения",
+    phone: "+7 999 999 99 99",
+    dialogLink: "https://vk.com",
+    price: 1500,
+    prepayment: 500,
+  },
+];
