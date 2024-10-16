44ba5475bf61d942a7be81a6244debc84ec8a558
Sergey Luzik <luziksergey@gmail.com>
2024-10-16 08:41:32 +0300

    ref: Field

--- src/components/blocks/Orders/Order/Field/index.jsx
+++ src/components/blocks/Orders/Order/Field/index.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./styles";
+import { OrderTextInput } from "../../../../ui/OrderTextInput";
 
-export const Field = ({ width, children }) => {
-  return <S.OrderItem $width={width}>{children}</S.OrderItem>;
+export const Field = ({ width, text, editable }) => {
+  const [textValue, setText] = useState(text);
+  const [editMode, setEditMode] = useState(false);
+
+  const handleRootClick = () => {
+    console.log("editMode", editMode);
+    setEditMode((prev) => (prev ? prev : !prev));
+  };
+
+  const handleValueChange = (value) => {
+    console.log("setting value by setText", value);
+    setText(value);
+
+    setEditMode(false);
+  };
+  return (
+    <S.Filed $width={width} onClick={handleRootClick}>
+      {editMode ? (
+        <OrderTextInput
+          value={textValue}
+          handleValueChange={handleValueChange}
+        />
+      ) : (
+        <S.Text>{textValue}</S.Text>
+      )}
+    </S.Filed>
+  );
 };
--- src/components/blocks/Orders/Order/Field/styles.js
+++ src/components/blocks/Orders/Order/Field/styles.js
@@ -1,5 +1,17 @@
 import styled from "styled-components";
+import { P } from "../../../../../styled-tags";
+import { pill } from "../../../../../style-fragments";
 
-export const OrderItem = styled.div`
+export const Filed = styled.div`
   width: ${({ $width }) => $width};
+`;
+
+export const Text = styled(P)`
+  ${pill};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.extraLiteMain};
+    cursor: pointer;
+    border-radius: ${({ theme }) => theme.radiuses.m};
+  }
 `;
