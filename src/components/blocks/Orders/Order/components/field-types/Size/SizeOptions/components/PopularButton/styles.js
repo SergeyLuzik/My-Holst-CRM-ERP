326ee2bccb63bc4749d8678b0620c49275048628
Sergey Luzik <luziksergey@gmail.com>
2024-10-21 05:57:26 +0300

    feat: add colors change when click on the PopularButton

--- src/components/blocks/Orders/Order/components/field-types/Size/SizeOptions/components/PopularButton/styles.js
+++ src/components/blocks/Orders/Order/components/field-types/Size/SizeOptions/components/PopularButton/styles.js
@@ -6,6 +6,11 @@ import fire from "@icons/fire.svg";
 export const PopularButton = styled(Button)`
   ${flexCenter};
   ${squareButton}
+  ${({ theme, $mode }) =>
+    $mode &&
+    `color: ${theme.colors.white}; background-color: ${theme.colors.main}; border-color: ${theme.colors.main}; &:hover {
+      background-color: ${theme.colors.main}; }
+    }`}
   &::before {
     width: 14px;
     height: 20px;
