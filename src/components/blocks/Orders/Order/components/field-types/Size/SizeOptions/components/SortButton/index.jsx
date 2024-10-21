b1ab67d117f5760f04d3fe3d90ff03d4a8fcb6f0
Sergey Luzik <luziksergey@gmail.com>
2024-10-21 05:54:47 +0300

    feat: add mode and onClick props to the SortButton

--- src/components/blocks/Orders/Order/components/field-types/Size/SizeOptions/components/SortButton/index.jsx
+++ src/components/blocks/Orders/Order/components/field-types/Size/SizeOptions/components/SortButton/index.jsx
@@ -1,3 +1,5 @@
 import React from "react";
 import * as S from "./styles";
-export const SortButton = () => <S.SortButton />;
+export const SortButton = ({ mode, onClick }) => (
+  <S.SortButton $mode={mode} onClick={onClick} />
+);
