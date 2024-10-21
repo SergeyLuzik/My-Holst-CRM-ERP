8ab03246d4daa3a553610313e22a22f4bdc78eaa
Sergey Luzik <luziksergey@gmail.com>
2024-10-21 05:58:48 +0300

    ref: move Sizes to the SizesOptions

--- src/components/blocks/Orders/Order/components/field-types/Size/index.jsx
+++ src/components/blocks/Orders/Order/components/field-types/Size/index.jsx
@@ -6,53 +6,6 @@ import { TextInput } from "../../common/TextInput";
 import { SizeOptions } from "./SizeOptions";
 import { searchTextMatch } from "@utils";
 
-const SIZES = {
-  allSizes: [
-    "15х18",
-    "15х20",
-    "18х27",
-    "20х25",
-    "20х30",
-    "25х25",
-    "25х30",
-    "25х35",
-    "30х30",
-    "30х35",
-    "30х40",
-    "30х45",
-    "35х35",
-    "35х40",
-    "35х45",
-    "35х50",
-    "40х40",
-    "40х45",
-    "40х50",
-    "40х60",
-    "45х55",
-    "45х70",
-    "50х50",
-    "50х60",
-    "50х65",
-    "50х70",
-    "50х75",
-    "60х60",
-    "60х70",
-    "60х80",
-    "60х90",
-    "70х70",
-    "70х90",
-    "80х80",
-    "80х95",
-    "80х100",
-    "80х120",
-    "90х135",
-    "100х120",
-    "100х130",
-    "100х150",
-  ],
-  popular: ["40х60", "30х40", "30х45", "40х50", "20х30", "35х50", "60х90"],
-};
-
 export const Size = ({ size }) => {
   const [top, setTop] = useState(0);
   const [left, setLeft] = useState(0);
@@ -94,11 +47,7 @@ export const Size = ({ size }) => {
             handleModeChange={handleModeChange}
           />
           <Popup top={top} left={left}>
-            <SizeOptions
-              text={text}
-              sizes={SIZES}
-              handleOptionClick={handleOptionClick}
-            />
+            <SizeOptions text={text} handleOptionClick={handleOptionClick} />
           </Popup>
         </>
       ) : (
