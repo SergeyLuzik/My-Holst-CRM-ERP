215192055a8bf50dedcc7d3de80d5d98880e3a09
SergeyLuzik <67701136+SergeyLuzik@users.noreply.github.com>
2024-10-24 11:19:29 +0000

    fix: typo in visually-hidden css fragment export

--- src/style-fragments/index.js
+++ src/style-fragments/index.js
@@ -1,4 +1,4 @@
 export { flexCenter } from "./flex-center";
 export { squareButton } from "./square-button";
 export { pill } from "./pill";
-export { visualHidden } from "./visual-hidden";
+export { visuallyHidden } from "./visually-hidden";
