53f0d69819823f8f7322abab0e82719bf2a7ec65
Sergey Luzik <luziksergey@gmail.com>
2024-10-24 06:05:08 +0300

    ref: add className prop to the Text

--- src/components/blocks/Orders/Order/components/common/Text/index.jsx
+++ src/components/blocks/Orders/Order/components/common/Text/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import * as S from "./styles";
 
-export const Text = ({ children, onClick }) => (
-  <S.Text onClick={onClick}>{children}</S.Text>
+export const Text = ({ children, onClick, className }) => (
+  <S.Text className={className} onClick={onClick}>
+    {children}
+  </S.Text>
 );
